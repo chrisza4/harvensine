92132b835cfe1e35c249ac3b3d26703c75702c71
Chakrit Riddhagni <chakrit.lj@gmail.com>
2025-05-20 15:58:31 +0700

    add proper comma

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@ mod generator;
 
 fn main() {
     let p = Path::new("output/output");
-    let sum = generator::generate(p, 1000000);
+    let sum = generator::generate(p, 1_000_000);
     println!("Expected sum: {}", sum.unwrap());
 }
